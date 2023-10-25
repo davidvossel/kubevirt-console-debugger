fb49bd1556a75479c62a5228540912f466759db4
David Vossel <davidvossel@gmail.com>
2023-10-25 13:18:29 -0400

    print date in UTC tz when connecting to console
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 while true; do
+	date -u
 	echo "attempting to connect to vmi $1 at namespace $2 streaming console log to stdout"
 	/usr/bin/virtctl console $1 --namespace $2
 	sleep 2
