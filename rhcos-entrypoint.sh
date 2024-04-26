68360a5aad399fed1e84b837f002511d2bb09682
David Vossel <davidvossel@gmail.com>
2024-04-26 14:18:49 -0400

    update entry point to point to correct binary
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- rhcos-entrypoint.sh
+++ rhcos-entrypoint.sh
@@ -3,7 +3,7 @@
 while true; do
 	date -u
 	echo "attempting to connect to vmi $1 at namespace $2 streaming console log to stdout"
-	/usr/bin/kubevirt-console-debugger $1 $2
+	/usr/bin/rhcos-console-debugger $1 $2
 	sleep 2
 done
 
